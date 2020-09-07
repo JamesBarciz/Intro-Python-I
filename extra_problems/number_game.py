79834b764cf4239ff9a1d68563b4d168bd1c647c
James Barciz <jamesjbarciz@gmail.com>
2020-09-06 23:31:07 -0400

    complete number game

--- extra_problems/number_game.py
+++ extra_problems/number_game.py
@@ -3,6 +3,8 @@
 # Program makes a guess
 # Tell it if the number is > or < than its guess
 # Uses that info to modify its guess until it homes in
+
+# This program has a pretty sharp learning rate
 import random
 import time
 
@@ -11,17 +13,21 @@ user_number = int(input('Select a number from 1-100: '))
 time.sleep(1)
 
 print('Computer is selecting a number...')
-time.sleep(2)
+time.sleep(1)
 
 computer_number = random.randint(1, 100)
 
-response_0 = input(f'Is your number {computer_number}? (yes/no) ')
-if response_0 == 'no':
-    response_1 = input(f'Is your number > or < than {computer_number}? ')
-    while response_1 == '>':
-        computer_number = random.randint(user_number + 1, 100)
+while True:
+    if user_number == computer_number:
+        print(f'Computer chooses: {computer_number}')
+        time.sleep(0.5)
+        print('Computer has selected your number!')
         break
-    while response_1 == '<':
-        computer_number = random.randint(1, user_number - 1)
-        break
-breakpoint()
+    else:
+        print(f'Computer chooses: {computer_number}')
+        time.sleep(0.5)
+        response = input(f'Is your number > or < than {computer_number}? ')
+        if response == '<':
+            computer_number = random.randint(user_number, computer_number - 1)
+        elif response == '>':
+            computer_number = random.randint(computer_number + 1, user_number)
